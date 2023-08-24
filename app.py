3c1eb386f68ad3e57fa0fce7804137350d2c7f1a
tanvir-ishraq <72716721+tanvir-ishraq@users.noreply.github.com>
2023-08-24 13:27:22 +0600

    flask deployment added

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        input_text = request.form['text']
+        output = predict_genres(input_text)[0]
+        confidence_list = output['confidences']
+        labels = [elem['label'] for elem in confidence_list if elem['confidence'] >= 0.23 ]
+        label_text = ""
+        for idx, label in enumerate(labels):
+            label_text = label_text + label
+            if idx != len(labels)-1: label_text = label_text + ", "
+        return render_template("result.html", input_text=input_text, output_text=label_text)
+    else:
+        return render_template("index.html")
+
+def predict_genres(input_text):
+    response = requests.post("https://tanvir-ishraq-quote-text-style-classifier.hf.space/run/predict", json={
+        "data": [
+            input_text
+        ]
+    }).json()
+    data = response["data"]
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True)
